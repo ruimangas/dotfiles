43689769d193bf848b4936c3157eee63acb707ea
ruijose <ruijosemangas@gmail.com>
2016-03-15 19:12:18 +0000

    install script added

---
+++ install.sh
@@ -0,0 +1,18 @@
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files=".bashrc .bash_aliases .tmux_conf .vim .vimrc .zshrc"
+
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+for file in $files; do
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
