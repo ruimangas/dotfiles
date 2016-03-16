8f3388a46681e4c7b7b1a88b28af5f81357708f6
ruijose <ruijosemangas@gmail.com>
2016-03-16 11:39:22 +0000

    install script

---
+++ install.rb
@@ -0,0 +1,40 @@
+puts "Ruby script to install the dot files into home directory"
+#heavily based on Ben Orenstein (https://github.com/r00k) rakefile
+
+def replace(file)
+  puts "removing file ~/.#{file}"
+  cmd = "rm '~/.#{file}'"
+  system(cmd)
+  create_symbolic_link(file)
+end
+
+def create_symbolic_link(file)
+  puts "creating symbolic link ~/.#{file}"
+  cmd = "ln -s ~/dotfiles/#{file} ~/.#{file}"
+  system(cmd)
+end
+
+Dir['*'].each do |file|
+  replace_all_files = false
+
+  if File.exists?(File.join(Dir.home, ".#{file}"))
+    if replace_all_files
+      replace(file)
+    else
+      print "overwrite ~/.#{file}? [ynaq] "
+      case gets.chomp
+      when 'a'
+        replace_all_files = true
+        replace(file)
+      when 'y'
+        replace(file)
+      when 'q'
+        exit
+      else
+        puts "skipping ~/.#{file}"
+      end
+    end
+  else
+    create_symbolic_link(file)
+  end
+end
